3858be11301e6ef4b10203ab2be151b074772b37
JimHiggins <jim242@hotmail.co.uk>
2020-03-24 14:29:30 +0000

    consume the context 2 ways

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        // Destructuring to get them as individual const
+        const { isLightTheme, light, dark } = this.context;
+        // ternary
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className='book-list'>
+            <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
                 <ul>
-                    <li>Book 1</li>
-                    <li>Book 2</li>
-                    <li>Book 3</li>
+                    <li style={{ background: theme.ui }}>Book 1</li>
+                    <li style={{ background: theme.ui }}>Book 2</li>
+                    <li style={{ background: theme.ui }}>Book 3</li>
                 </ul>
             </div>
         );
--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                // Destructuring to get them as individual const
+                const { isLightTheme, light, dark } = context;
+                // ternary
+                const theme = isLightTheme ? light : dark;
+                return(
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                )
+            }}</ThemeContext.Consumer>
         );
     }
 }
