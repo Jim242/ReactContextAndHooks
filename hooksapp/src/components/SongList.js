70ed90cc40e6ac40b9a57f298390ec5d03ab6eea
Jim Higgins <jim242@hotmail.co.uk>
2020-03-25 10:56:04 +0000

    useState hook added

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+       { title: 'Song 1', id: 1 },
+       { title: 'Song 2', id: 2 },
+       { title: 'Song 3', id: 3 } 
+    ]);
+
+    const addSong = () => {
+        setSongs([...songs, { title: 'new song', id: uuid() }])
+    }
     return (
         <div className="song-list">
             <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
+                {songs.map(song => {
+                    return ( 
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
             </ul>
+            <button onClick={addSong}>Add new song</button>
         </div>
     );
 }
